1d63dfe1c294f2da43bc33af4c9b1e1cb726f284
Roald Niels Medendorp <78323364+Roald-Niels@users.noreply.github.com>
2022-08-31 13:13:14 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+//Roald Niels Medendorp//
+//8/29/2022//
+//Module 1 Programming Assignment//
+//In Java, the division for integers is the quotient—the fractional part is cut short"//
+public class Main {
+  public static void main(String[] args) {
+    System.out.print("This equation is done with decimal numbers and becomes:");
+    
+    System.out.println(4 * ( 1.0 - 1.0 / 3.0 + 1.0 / 5.0 - 1.0 / 7.0 + 1.0 / 9.0 - 1.0 / 11.0 + 1.0 / 13.0 ));
+    
+    System.out.print("This equation is done without decimals and becomes:");
+    
+    System.out.println(4 * ( 1 - 1 / 3 + 1 / 5 - 1 / 7 + 1 / 9 - 1 / 11 + 1 / 13 ));
+  }
+}
+
